b83c249ab4b16e15a830b7d8b9dead2e309ef9f5
Krish Kalla <116767392+KrishKalla@users.noreply.github.com>
2023-03-27 11:18:19 -0400

    Create Board method defined

--- Connect4.py
+++ Connect4.py
@@ -1,1 +1,19 @@
+import numpy as np
+import math
+import pygame as pg
+import sys
 
+RowNumber = 6
+ColumnCount = 7
+
+
+
+
+
+
+def create_board():
+    board = np.zeros((6,7))
+    return board
+
+board = create_board()
+print(board)
